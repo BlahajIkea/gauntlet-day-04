24cca65584eca88e7d05f856ff843aa0e2a7df21
Mikael <mikaeltubetv@gmail.com>
2026-08-12 12:15:01 +0800

    init commit

---
+++ ex00/entity.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+
+struct Entity { int id; std::string name; int hp; };
+
+
+class Entity {
+
+
+};
+
+/*
+// seed, in this order:
+int id; std::string name; int hp;
+{7, "orc", 30}, {2, "bat", 8}, {42, "boss", 300}, {13, "rat", 4}
+*/
---
+++ ex00/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "entity.hpp"
+
+struct Entity { int id; std::string name; int hp; };
+
+int main() 
+{
+ 
+    
+
+
+    // seed:
+    {7, "orc", 30}, {2, "bat", 8}, {42, "boss", 300}, {13, "rat", 4}
+
+}
